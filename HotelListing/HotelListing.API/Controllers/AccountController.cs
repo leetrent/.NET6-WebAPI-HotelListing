6dfaafd3a09743bf586e61ec906eaf566f4048e4
LeeTrent <lee.trent.1@gmail.com>
2022-10-24 06:12:05 -0500

    Added logging to AccountController

--- HotelListing/HotelListing.API/Controllers/AccountController.cs
+++ HotelListing/HotelListing.API/Controllers/AccountController.cs
@@ -10,11 +10,14 @@ namespace HotelListing.API.Controllers
     public class AccountController : ControllerBase
     {
         private readonly IAuthService _authService;
+        private readonly ILogger<AccountController> _logger;
 
-        public AccountController(IAuthService authService)
+        public AccountController(IAuthService authService, ILogger<AccountController> logger)
         {
             _authService = authService;
+            _logger = logger;
         }
+
 
         // POST: api/Account/register
         [HttpPost]
@@ -26,6 +29,8 @@ namespace HotelListing.API.Controllers
         {
             try
             {
+                _logger.LogInformation("Registration attempt by '{Email}'", userDTO.Email);
+
                 IEnumerable<IdentityError> errors = await _authService.RegisterUser(userDTO);
                 if (errors.Any())
                 {
@@ -33,23 +38,23 @@ namespace HotelListing.API.Controllers
                     {
                         ModelState.AddModelError(error.Code, error.Description);
                     }
+                    _logger.LogInformation("Registration attempt by '{Email}' generated a 'BadRequest' : '{ModelState}'", userDTO.Email, ModelState);
                     return BadRequest(ModelState);
                 }
+                _logger.LogInformation("Registration attempt by '{Email}' was successful", userDTO.Email);
                 return Ok();
             }
             catch (Exception exc)
             {
-                Console.WriteLine();
-                Console.WriteLine(exc.Message);
-                Console.WriteLine();
-                Console.WriteLine(exc.StackTrace);
-                Console.WriteLine();
-                return Problem("An unexpected error occurred on our end.");
+                _logger.LogError("Registration attempt by '{userDTO.Email}' failed", userDTO.Email);
+                _logger.LogError("Exception Message: {Message}", exc.Message);
+                _logger.LogError("Exception StackTrace: {StackTrace}", exc.StackTrace);
+                return Problem("An unexpected error occurred on our end during the registration process.", statusCode: 500);
             }
         }
 
         // POST: api/Account/login
-        [HttpPost]
+        [HttpPost] 
         [Route("login")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -58,21 +63,23 @@ namespace HotelListing.API.Controllers
         {
             try
             {
+                _logger.LogInformation("Login attempt by '{Email}'", loginDTO.Email);
+
                 TokenDTO? tokenDTO = await _authService.Login(loginDTO);
                 if (tokenDTO == null)
                 {
+                    _logger.LogInformation("Login attempt failed because '{Email}' is 'unauthorized'", loginDTO.Email);
                     return Unauthorized();
                 }
+                _logger.LogInformation("Login attempt by '{Email}' was successful.", loginDTO.Email);
                 return Ok(tokenDTO);
             }
             catch (Exception exc)
             {
-                Console.WriteLine();
-                Console.WriteLine(exc.Message);
-                Console.WriteLine();
-                Console.WriteLine(exc.StackTrace);
-                Console.WriteLine();
-                return Problem("An unexpected error occurred on our end.");
+                _logger.LogError("Login attempt by '{loginDTO.Email}' failed", loginDTO.Email);
+                _logger.LogError("Exception Message: {Message}", exc.Message);
+                _logger.LogError("Exception StackTrace: {StackTrace}", exc.StackTrace);
+                return Problem("An unexpected error occurred on our end during the login process.", statusCode: 500);
             }
         }
 
@@ -84,14 +91,23 @@ namespace HotelListing.API.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult> RefreshToken([FromBody] TokenDTO oldToken)
         {
-            TokenDTO? newToken = await _authService.RefreshToken(oldToken);
-
-            if (newToken == null)
+            try
             {
-                return Unauthorized();
-            }
+                TokenDTO? newToken = await _authService.RefreshToken(oldToken);
 
-            return Ok(newToken);
+                if (newToken == null)
+                {
+                    return Unauthorized();
+                }
+
+                return Ok(newToken);
+            }
+            catch (Exception exc)
+            {
+                _logger.LogError("Exception Message: {Message}", exc.Message);
+                _logger.LogError("Exception StackTrace: {StackTrace}", exc.StackTrace);
+                return Problem("An unexpected error occurred on our end during the 'refresh token' process.", statusCode: 500);
+            }
         }
     }
 }
